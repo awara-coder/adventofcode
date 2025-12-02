217b4cc1b7f424dc4e11eb86004d27bf9307e2ac
Sagar Gupta <sagar.june97p@gmail.com>
2025-12-02 20:10:56 +0530

    chore: created go project

---
+++ 2025/go_solution/go.mod
@@ -0,0 +1,3 @@
+module github.com/awara-coder/adventofcode/2025/go_solution
+
+go 1.25.3
---
+++ 2025/rust_solution/target/debug/deps/rust_solution-0fb941a6770a57c0.d
@@ -0,0 +1,5 @@
+/Users/sagargupta/selfrepos/adventofcode/2025/rust_solution/target/debug/deps/rust_solution-0fb941a6770a57c0.d: src/main.rs
+
+/Users/sagargupta/selfrepos/adventofcode/2025/rust_solution/target/debug/deps/librust_solution-0fb941a6770a57c0.rmeta: src/main.rs
+
+src/main.rs:
